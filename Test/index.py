070d78200e706a537405ac97965711ff7654c3b9
SanidhyaMishra2021 <sanidhyamishra22021@gmail.com>
2021-10-09 22:29:58 +0530

    dark_theme

--- Test/index.py
+++ Test/index.py
@@ -2,3 +2,12 @@ print ("Higi tpsuI am Sanidhya Mishra.")
 
 print ("I am a student og Vit Bhopal")
 print ( "I love coding")
+print ("25462")
+print ("Dark theme")
+
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+
+
+c = a+b
+print(c)
